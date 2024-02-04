2b29816630d5a29cbfdce30a265b1574154ce82b
AhmadSealights <ahmad.wahbi@sealights.io>
2024-02-04 15:53:51 +0200

    added second api and moved to package

--- src/main/scala/server/WebApp.scala
+++ src/main/scala/server/WebApp.scala
@@ -1,13 +1,16 @@
+package server
+
 import cask.MainRoutes
+
 
 object WebApp extends MainRoutes {
     @cask.get("/")
     def hello() = {
         "Hello, World!"
     }
-    @cask.get("/")
-    def hello() = {
-        "Hello, World!"
+    @cask.get("/s/:name")
+    def s(name: String) = {
+        "Hello, "+name+"!"
     }
 
     override def port: Int = 8080
