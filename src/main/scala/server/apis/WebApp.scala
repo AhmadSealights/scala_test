8156914a6a1d2d650b6d868a58c93b2a9bde78d0
AhmadSealights <ahmad.wahbi@sealights.io>
2024-02-05 09:15:46 +0200

    prepare for

--- src/main/scala/server/apis/WebApp.scala
+++ src/main/scala/server/apis/WebApp.scala
@@ -1,4 +1,4 @@
-package server
+package server.apis
 
 import cask.MainRoutes
 
@@ -10,7 +10,7 @@ object WebApp extends MainRoutes {
     }
     @cask.get("/s/:name")
     def s(name: String) = {
-        "Hello, "+name+"!"
+        "Hello, "+name
     }
 
     override def port: Int = 8080
