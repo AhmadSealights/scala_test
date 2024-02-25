2019c7521b053abf6d650089da80d9d75d2674e0
AhmadSealights <ahmad.wahbi@sealights.io>
2024-02-25 12:40:51 +0200

    added some more coverage

--- app/src/main/scala/server/functions/externalfunctions.scala
+++ app/src/main/scala/server/functions/externalfunctions.scala
@@ -7,9 +7,11 @@ object  externalfunctions{
     def caseclass(n: String,a: Int):String = {
         case class Person(name: String, age: Int)
         val person = Person(n, a)
-
+        //These two methods are to get coverage of the methods inhereted by the product class
+        val namefield = person.productElementNames.next()//returns "Name"
+        val agefield=person.productElementName(0)// returns "Age"
         person match {
-        case Person(name, age) => return(s"Name: $name, Age: $age")
+        case Person(name, age) => return(s"$namefield: $name, $agefield: $age")
         }
     }
 }
